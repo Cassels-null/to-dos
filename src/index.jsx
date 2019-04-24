ad42c331a3c2febde8626ee2579f09ce860bf874
Cassels-null <42cassels@gmail.com>
2019-04-24 17:05:07 -0400

    pretify layout

--- src/index.jsx
+++ src/index.jsx
@@ -16,15 +16,25 @@ class App extends React.Component {
     addToList(e){
         e.preventDefault();
         let tempList = this.state.list.slice();
-        tempList.push(document.getElementById("taskInput").value);
+        tempList.push({
+            task: document.getElementById("taskInput").value,
+            done: false});
         this.setState({list: tempList});
         // window.alert(this.state.list.length);
     }
 
+    // toggleDone(id){
+    //     let targetIndex = this.list.findIndex((ele)=>{ele.id ===id})
+    //     if(targetIndex < 0) return;
+    //     let tempList = this.state.list.slice();
+    //     templist[targetIndex].done = true;
+    //     this.setState({list: tempList});
+    // }
+
     render(){
-        return(<div>
-                One Cannot Eat Raw Sparklebunny
-                    <form onSubmit={this.addToList}>
+        return(<div id="main">
+                <div>One Cannot Eat Raw Sparklebunny</div>
+                    <form id="inputBar" onSubmit={this.addToList}>
                     <input id="taskInput" type="text" placeholder="create new to-do"></input>
                     <input id="submit" type="submit" value="add to-do"></input>
                 </form>
--- src/listItem.jsx
+++ src/listItem.jsx
@@ -4,13 +4,27 @@ import ReactDOM from 'react-dom';
 class ListItem extends React.Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            done: this.props.done
+        }
     }
 
     render(){
         return(
-            <div className="listItem">{this.props.item}</div>
+            <div className="listItem">
+                <span>{(this.state.done === true)? 
+                    <s>{this.props.item.task}</s> : 
+                    this.props.item.task
+                }</span>
+                <input type="checkbox" onSubmit={this.toggleDone}/>
+            </div>
         )
+    }
+
+    toggleDone(){
+        if(this.state.done === false){
+            this.setState({done: true})
+        }
     }
 }
 export default ListItem;
