1ef63ccc368ae869387074d79f8a3a5b1e77a6c0
Cassels-null <42cassels@gmail.com>
2019-04-23 23:48:16 -0400

    style details

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import NewTodo from './newTodo.jsx';
+
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            list: []
+        }
+        this.addToList = this.addToList.bind(this);
+    }
+
+    addToList(task){
+        window.alert("_______");
+        this.state.list.push(task);
     }
 
     render(){
         return(<div>
                 One Cannot Eat Raw Sparklebunny
+                <NewTodo addToList={this.addToList}/>
             </div>
         )
     }
+
+
 }
 // window.alert("ping!");
 ReactDOM.render(<App/>, document.getElementById("app"));
